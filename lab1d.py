48014bc4d7e213e30f9e1c63ec3c9c32f3a10892
Firstname Lastname <kppatel47@myseneca.ca>
2025-09-12 14:42:42 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Objects
+x = 10
+y = 2
+z = 5
+
+# Expression
+result = x + y * z
+
+# Print the equation and the result
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(result))
